19fbf567d8fc84212599c2cbd226af219ff1db40
jinal chauhan <jinalchauhan454@gmail.com>
2023-08-21 15:54:40 +0530

    {modify}dashboard

--- index.php
+++ index.php
@@ -265,7 +265,7 @@ if ($total != 0)
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0 text-dark">Dashboard device</h1>
+              <h1 class="m-0 text-dark">Dashboard </h1>
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
