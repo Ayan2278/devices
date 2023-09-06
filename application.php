476936bfe7f200e1db7cbfa3a363e6f5b61c8d5e
jinal chauhan <jinalchauhan454@gmail.com>
2023-09-06 15:26:06 +0530

    t

--- application.php
+++ application.php
@@ -73,6 +73,7 @@ if (isset($_POST['DIST']) && isset($_POST['Block']) && isset($_POST['Village']) 
     $file = "JSON PC/" . $_POST['PC'] . ".json";
     $data = file_get_contents($file);
     $data = json_decode($data, true);
+    $data = array_unique($data, true);
     $cd++;
 
   }
