195c5c1c7ace137e36256f2a33970fa8be40989c
jinal chauhan <jinalchauhan454@gmail.com>
2023-09-13 16:03:33 +0530

    t

--- index.php
+++ index.php
@@ -390,7 +390,7 @@ function status($pcNo)
   <!-- overlayScrollbars -->
   <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
   <!-- AdminLTE App -->
-  <!-- <script src="dist/js/adminlte.js"></script> -->
+  <script src="dist/js/adminlte.js"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
   <script src="dist/js/pages/dashboard.js"></script>
   <!-- AdminLTE for demo purposes -->
