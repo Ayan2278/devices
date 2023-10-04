8cfa5e0585ffe2fc229b3792ec80d685f96271d0
MrSky099 <ayanchhipa2278@gmail.com>
2023-10-04 17:55:24 +0530

    a

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ $client->setClientId('402201038344-l08vsoiqjnuoje4amh2ppbrkkjgb6slc.apps.googleu
 // Enter your Client Secrect
 $client->setClientSecret('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');
 // Enter the Redirect URL
-$client->setRedirectUri('http://localhost/GitHub/devices/index.php');
+$client->setRedirectUri('http://localhost/Devices/devices/index.php');
 
 // Adding those scopes which we want to get (email & profile Information)
 $client->addScope("email");
@@ -37,7 +37,7 @@ if (isset($_GET['code'])):
 
         // Storing data into database
         $id = mysqli_real_escape_string($conn, $google_account_info->id);
-        $full_name = mysqli_real_escape_string($db_connection, trim($google_account_info->name));
+        $full_name = mysqli_real_escape_string($conn, trim($google_account_info->name));
         $email = mysqli_real_escape_string($db_connection, $google_account_info->email);
         $profile_pic = mysqli_real_escape_string($db_connection, $google_account_info->picture);
 
@@ -120,6 +120,68 @@ else:
                 color: black;
             }
         </style>
+         <style>
+        *,
+        *::before,
+        *::after {
+            box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+        }
+        body{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #f7f7ff;
+            padding: 10px;
+            margin: 0;
+        }
+        ._container{
+            max-width: 400px;
+            background-color: #ffffff;
+            padding: 20px;
+            margin: 0 auto;
+            border: 1px solid #cccccc;
+            border-radius: 2px;
+        }
+        ._container.btn{
+            text-align: center;
+        }
+        .heading{
+            text-align: center;
+            color: #4d4d4d;
+            text-transform: uppercase;
+        }
+        .login-with-google-btn {
+            transition: background-color 0.3s, box-shadow 0.3s;
+            padding: 12px 16px 12px 42px;
+            border: none;
+            border-radius: 3px;
+            box-shadow: 0 -1px 0 rgb(0 0 0 / 4%), 0 1px 1px rgb(0 0 0 / 25%);
+            color: #ffffff;
+            font-size: 14px;
+            font-weight: 500;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+            background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=);
+            background-color: #4a4a4a;
+            background-repeat: no-repeat;
+            background-position: 12px 11px;
+            text-decoration: none;
+        }
+        .login-with-google-btn:hover {
+            box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25);
+        }
+        .login-with-google-btn:active {
+            background-color: #000000;
+        }
+        .login-with-google-btn:focus {
+            outline: none;
+            box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25), 0 0 0 3px #c8dafc;
+        }
+        .login-with-google-btn:disabled {
+            filter: grayscale(100%);
+            background-color: #ebebeb;
+            box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+            cursor: not-allowed;
+        }
+    </style>
     </head>
 
     <body class="bg">
@@ -168,14 +230,14 @@ else:
                                     </div>
                                 </div>
                                 <div class="form-group col-lg-12">
-                                    <div class="_container btn">
+                                    <!-- <div class="_container btn"> -->
 
-                                        <a type="button" class="login-with-google-btn"
+                                        <a type="button" class="login-with-google-btn col-lg-11 mx-2"
                                             href="<?php echo $client->createAuthUrl(); ?>">
                                             Sign in with Google
                                         </a>
 
-                                    </div>
+                                    <!-- </div> -->
                                 </div>
 
                                 <div class="form-group col-lg-12">
